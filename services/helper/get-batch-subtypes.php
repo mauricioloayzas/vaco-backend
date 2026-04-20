9c783e61111755f99fd5d23b964cd6059fc6659f
Mauricio Loayza <mauricioloayzas@mauloasan.com>
2026-04-19 21:27:31 -0500

    add a query param to get the subtypes by a type

--- services/helper/get-batch-subtypes.php
+++ services/helper/get-batch-subtypes.php
@@ -1,16 +1,34 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
 
-use Mauloasan\BobConstruye\DynamoDB\Enums\Vaco\BatchSubtype;
+use Mauloasan\BobConstruye\DynamoDB\Enums\Vaco\BatchType;
 
 return function (array $event) {
 
     try {
 
+        $typeValue = $event['queryStringParameters']['type'] ?? null;
+
+        if (!$typeValue) {
+            return [
+                'statusCode' => 400,
+                'body' => json_encode(['error' => 'Missing required parameter: type'])
+            ];
+        }
+
+        $batchType = BatchType::tryFrom($typeValue);
+
+        if (!$batchType) {
+            return [
+                'statusCode' => 400,
+                'body' => json_encode(['error' => "Invalid type: $typeValue"])
+            ];
+        }
+
         $data = array_map(fn($case) => [
             'value' => $case->value,
             'label' => $case->name,
-        ], BatchSubtype::cases());
+        ], $batchType->getSubtypes());
 
         return [
             'statusCode' => 200,
