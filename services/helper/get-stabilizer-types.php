d4279dc18171701274194dbb90490100fd72571f
Mauricio Loayza <mauricioloayzas@mauloasan.com>
2026-05-13 16:15:38 -0500

    big changes to calculate the price cost in the mead recipe

---
+++ services/helper/get-stabilizer-types.php
@@ -0,0 +1,28 @@
+<?php
+require_once file_exists('/opt/vendor/autoload.php') ? '/opt/vendor/autoload.php' : __DIR__ . '/../../vendor/autoload.php';
+
+use Mauloasan\BobConstruye\DynamoDB\Enums\Vaco\StabilizerType;
+
+return function (array $event) {
+
+    try {
+
+        $data = array_map(fn($case) => [
+            'value' => $case->value,
+            'label' => $case->name,
+        ], StabilizerType::cases());
+
+        return [
+            'statusCode' => 200,
+            'headers' => ['Content-Type' => 'application/json'],
+            'body' => json_encode(['data' => $data])
+        ];
+
+    } catch (Exception $e) {
+
+        return [
+            'statusCode' => 500,
+            'body' => json_encode(['error' => $e->getMessage()])
+        ];
+    }
+};
